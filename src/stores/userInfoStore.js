ec488002d28569b8273e021dcf6862b184a8ef2f
s12171934 <ssong99999@naver.com>
2024-11-15 21:39:36 +0900

    [FEAT] 유저 상태 관리

--- src/stores/userInfoStore.js
+++ src/stores/userInfoStore.js
@@ -1,37 +1,54 @@
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import { SERVER_URL } from '@/assets/resources/configs/config';
+import axios from 'axios';
 
 export const useUserInfoStore = defineStore('userInfo', {
     state: () => ({
         user: null,
+        access_token: null, // access token을 userinfo에 저장
     }),
 
     getters: {
         getUser() {
+            console.log(this.user) //이후에 /api/user/info 에서 받아올 유저 정보 변경 필요
             return this.user;
         },
     },
 
     actions: {
-        setUser() {
-            if (document.cookie) {
-                const refreshToken = document.cookie
-                  .split('; ')
-                  .find(row => row.startsWith('refresh='))
-                  ?.split('=')[1];
-        
-                this.user = this.parseJWT(refreshToken);
-                // console.log(this.user);
+        // access token을 통해서 user 정보 받아오는 함수
+        setUser(access_token) {
+            if (access_token) {
+                this.access_token = access_token;
             }
+            const getUserInfo = async (times) => {
+                //times는 토큰 재발급 시도 횟수
+                if(times === 0 || !access_token) return;
+                axios.get(`${SERVER_URL}/api/user/info`, {
+                headers: {
+                    'Authorization': access_token
+                    }
+                }).then((res) => {
+                    this.user = res.data
+                }).catch(async (err) => {
+                    // 이 부분이 중요한데 access token이 만료되면 reissue하는 로직
+                    // 거의 모든 axios 요청에 대해서 들어가야 하는 부분인데 어떻게 처리하면 좋을까?
+                    console.log(err)
+                    if(err.response.status === 401) {
+                        console.log('토큰 재발급')
+                        await reissueAccessToken();
+                        getUserInfo(times - 1);
+                    }
+                })
+            }
+            getUserInfo(1);
         },
-        
-        parseJWT(token) {
-            const base64Url = token.split('.')[1]; // Payload 부분 추출
-            const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/'); // Base64Url을 Base64로 변환
-            const jsonPayload = decodeURIComponent(escape(window.atob(base64))); // Base64 디코드
-
-            return JSON.parse(jsonPayload); // JSON 객체로 변환
-        },  
+        // refresh token을 통해 access token 재발급 함수
+        reissueAccessToken: async () => {
+            axios.defaults.withCredentials = true;
+            const response = await axios.post(`${SERVER_URL}/api/auth/reissue`)
+            this.access_token = response.headers.authorization;
+        }
     },
 });
 
