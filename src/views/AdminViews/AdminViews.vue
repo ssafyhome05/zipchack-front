42cf3947fe41d564d12c9dde3c96199fac893786
s12171934 <ssong99999@naver.com>
2024-11-21 17:35:22 +0900

    [FEAT] 관리자 페이지와 관련된 화면 뷰 컴포넌트 제공

---
+++ src/views/AdminViews/AdminViews.vue
@@ -0,0 +1,39 @@
+<template>
+  <header></header>
+  <div class="admin-wrap">
+    <AdminSidebarComponent />
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+import AdminSidebarComponent from '@/components/adminComponents/AdminSidebarComponent.vue';
+</script>
+
+<style scoped>
+header{
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 84px;
+  background-color: #fff;
+  z-index: 2;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+}
+
+.admin-wrap {
+  background-color: #fff;
+  display: flex;
+}
+
+.admin-wrap > :nth-child(2) {
+  margin-left: 20vw;
+  margin-top: 84px;
+  flex: 1;
+}
+
+.admin-wrap * {
+  z-index: 2;
+}
+</style>
---
+++ src/views/AdminViews/DashBoardView.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="dashboard-container">
+        <SectionContainer :item="sectionList[0]" />
+        <div class="bottom-container">
+            <SectionContainer :item="sectionList[1]" />
+            <SectionContainer :item="sectionList[2]" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import SectionContainer from '@/components/adminComponents/SectionContainer.vue';
+import BoardTable from '@/components/adminComponents/BoardTable.vue';
+import TaskProgressCircle from '@/components/adminComponents/TaskProgressCircle.vue';
+import TaskBasic from '@/components/adminComponents/TaskBasic.vue';
+
+const sectionList = ref([
+    {
+        title: '공지사항',
+        component: BoardTable
+    },
+    {
+        title: '매물 거래내역 갱신',
+        component: TaskProgressCircle
+    },
+    {
+        title: '지역 별 통계 정보 갱신',
+        component: TaskBasic
+    }
+])
+
+</script>
+
+<style scoped>
+.dashboard-container {
+    padding: 20px;
+    background-color: #f5f5f5;
+    height: calc(100vh - 84px);
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+}
+
+.bottom-container {
+    display: flex;
+    gap: 20px;
+    height: calc((100% - 20px) / 2); /* 상단 컨테이너와 동일한 높이 */
+}
+
+/* 상단 테이블 컨테이너 */
+.dashboard-container > .section-container {
+    height: calc((100% - 20px) / 2);
+}
+
+/* 하단 테이블 컨테이너들 */
+.bottom-container > .section-container {
+    width: 50%;
+    height: 100%;
+}
+</style>
