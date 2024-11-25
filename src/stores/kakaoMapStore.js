3b0cf79445ee372e945c7eda89fc10cd6514223f
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2024-11-25 14:28:16 +0900

    Fix/marker rendering (#21)
    * [FIX] 새로운 지역 이동시 마커로 인한 로딩속도 느려짐 개선
    
    * [FIX] 편의시설 마커 삭제 딜레이 감소

--- src/stores/kakaoMapStore.js
+++ src/stores/kakaoMapStore.js
@@ -9,5 +9,18 @@ export const useKakaoMapStore = defineStore('kakaoMap', {
         setMapInstance(map) {
             this.mapInstance = map;  // 맵 인스턴스를 설정
         },
+
+        async resetMap() {
+            const map = this.mapInstance;
+      
+            if (map) {
+              // 기존 맵 초기화
+              const container = document.getElementById('map');
+              container.innerHTML = '';
+      
+              // 새 지도 생성
+            //   initializeMap();
+            }
+          }
     },
 });
