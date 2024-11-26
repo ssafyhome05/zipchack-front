f55d60c07876e25bf9e558cbf6e07239933585a8
hkPark0616 <qkrgorn3617@naver.com>
2024-11-26 11:03:54 +0900

    [FIX] aiStore token reissue

--- src/stores/aiStore.js
+++ src/stores/aiStore.js
@@ -3,6 +3,7 @@ import { SERVER_URL } from '@/assets/resources/configs/config';
 import { ref } from 'vue';
 import axios from 'axios';
 import { useUserInfoStore } from './userInfoStore';
+import { reissueAccessToken } from '@/assets/js/PlatformViewsScript/CommonScripts/reissueAccessToken';
 
 export const useAiStore = defineStore('aiStore', {
     state: () => ({
@@ -27,22 +28,34 @@ export const useAiStore = defineStore('aiStore', {
             console.log("싱행은 됨");
             console.log(promptResourceDto)
             try {
-                await axios.post(`${SERVER_URL}/api/ai/house`, promptResourceDto, {
+                const response = await axios.post(`${SERVER_URL}/api/ai/house`, promptResourceDto, {
                     headers: {
                         'Authorization': accessToken
                     },
 
+                });
+
+                if(response.data.code === 200050){
+                    this.response = "집착이 추천하는 집은 " + response.data.data.aptName +
+                                "입니다. \n\n가장 큰 이유 : "+ response.data.data.reason +
+                                "\n\n 상세설명 : " + response.data.data.description;
                 }
-                    )
-                    .then(response => {
-                        if(response.data.code === 200050){
-                            this.response = "집착이 추천하는 집은 " + response.data.data.aptName +
-                                        "입니다. \n\n가장 큰 이유 : "+ response.data.data.reason +
-                                        "\n\n 상세설명 : " + response.data.data.description;
-                        }
-                    }); 
+                if(response.data.code === 401012 || response.data.code === 401011){
+                    await reissueAccessToken();
+                    return this.setAiResponse(promptResourceDto);
+                }
+                    // )
+                    // .then(response => {
+                    //     if(response.data.code === 200050){
+                    //         this.response = "집착이 추천하는 집은 " + response.data.data.aptName +
+                    //                     "입니다. \n\n가장 큰 이유 : "+ response.data.data.reason +
+                    //                     "\n\n 상세설명 : " + response.data.data.description;
+                    //     }
+                    // }); 
             } catch (error) {
                 console.error("데이터 가져오기에 실패했습니다:", error);
+                await reissueAccessToken();
+                return this.setAiResponse(promptResourceDto);
             }
         },
     },
